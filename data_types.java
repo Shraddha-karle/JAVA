7becc0fcb40629f7461d28704e06063478116a72
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-01-27 13:22:29 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class data_types {
+    public static void main(String[] args) {
+        int a=23;
+        char b='z';
+        float c=2.3f;
+        double d=2.00;
+        short f=0;
+        boolean e= true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(e);
+    }
+}
