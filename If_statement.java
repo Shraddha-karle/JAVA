45efbb0a119b91c0835177877dbd0097c1b29b20
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-01-31 11:57:55 +0530

    Add files via upload

---
+++ If_statement.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class If_statement {
+    public static void main(String[] args) {
+        int a = 25;
+        if (a < 25) {
+            System.out.println("False");
+        }
+    }
+}
