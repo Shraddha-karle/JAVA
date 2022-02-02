13537a342fcf9abdf29ca0df5e1ffac75aea57a9
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-02-02 11:53:07 +0530

    Add files via upload

---
+++ If_else_satement.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class If_else_satement {
+    public static void main(String[] args) {
+        int age=25;
+        if(age>=18){
+            System.out.println("Your are applicable for license");
+        }
+        else{
+            System.out.println("You are not applicable for license");
+        }
+
+    }
+}
+
+
+
