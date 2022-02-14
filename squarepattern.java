e95480b1bedc3c7804e9d7ad5014ed2d839c9493
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-02-14 10:46:03 +0530

    Add files via upload

---
+++ squarepattern.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class squarepattern {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
